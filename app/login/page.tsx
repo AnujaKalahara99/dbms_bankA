d457ac518a4ec0f7dc422bd00ebc652cd6ca9728
AnujaKalahara99 <a.p.kalhara@gmail.com>
2024-09-23 23:13:17 +0530

    added mock credential details to login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,13 +4,39 @@ import LoginForm from "@/app/ui/login-form";
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
-      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+      <div className="relative flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
         <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
           <div className="w-32 text-white md:w-36">
             <BankALogo />
           </div>
         </div>
         <LoginForm />
+      </div>
+      <div className="relative flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+        <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
+          <h1 className={`mb-3 text-xl`}>Mock Login Credentials</h1>
+          <div className="w-full py-2">
+            <b>Customer Emails</b>
+            <p>
+              use <u>john.doe@example.com</u> or <u>jane.smith@example.com</u>{" "}
+              with password <i>customer</i>
+            </p>
+          </div>
+          <div className="w-full py-2">
+            <b>Employee Email</b>
+            <p>
+              use <u>bob.brown@example.com</u>
+              with password <i>employee</i>
+            </p>
+          </div>
+          <div className="w-full py-2">
+            <b>Manager Email</b>
+            <p>
+              use <u>alice.green@example.com</u>
+              with password <i>employee</i>
+            </p>
+          </div>
+        </div>
       </div>
     </main>
   );
